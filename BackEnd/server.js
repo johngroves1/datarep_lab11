98bec7fa44850fcbfbd75f3fd98c30fd2190992f
johngroves1 <G00367771@gmit.ie>
2020-11-20 17:28:37 +0000

    Lab Upload

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 4000
 const cors = require('cors')
 const bodyParser = require("body-parser")
+const mongoose = require('mongoose');
+
 
 // Read JSON data from the node server
 app.use(cors());
@@ -15,35 +17,66 @@ next();
 });
 
 
+
+
 // Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // Parse application/json
 app.use(bodyParser.json())
 
+// Connecting to Mongo server
+const myConnectionString = 'mongodb+srv://admin:gibbons99@cluster0.avqcw.mongodb.net/movies?retryWrites=true&w=majority'; 
+mongoose.connect(myConnectionString, {useNewUrlParser: true});
+
+// Defining schema
+const Schema = mongoose.Schema;
+
+var movieSchema = new Schema({
+    title:String,
+    year:String,
+    poster:String
+})
+
+var MovieModel = mongoose.model("movie", movieSchema);
+
 
 app.get('/api/movies', (req, res)=>{
 
-    const mymovies = [
-        {
-            "Title":"Avengers: Infinity War",
-            "Year":"2018",
-            "imdbID":"tt4154756",
-            "Type":"movie",
-            "Poster":"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title":"Captain America: Civil War",
-            "Year":"2016",
-            "imdbID":"tt3498820",
-            "Type":"movie",
-            "Poster":"https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            }
-    ];
+    // const mymovies = [
+    //     {
+    //         "Title":"Avengers: Infinity War",
+    //         "Year":"2018",
+    //         "imdbID":"tt4154756",
+    //         "Type":"movie",
+    //         "Poster":"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+    //         },
+    //         {
+    //         "Title":"Captain America: Civil War",
+    //         "Year":"2016",
+    //         "imdbID":"tt3498820",
+    //         "Type":"movie",
+    //         "Poster":"https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+    //         }
+    // ];
+
+    MovieModel.find((err, data)=>{
+        res.json(data);
+    })
+
     // Pass down to the server
-    res.status(200).json({
-        message: "Everything is okay",
-        movies:mymovies});
+    // res.status(200).json({
+    //     message: "Everything is okay",
+    //     movies:mymovies});
+})
+
+// Return back movie at specific id
+app.get('/api/movies/:id', (req, res)=>{
+    console.log(req.params.id);
+
+    MovieModel.findById(req.params.id, (err, data)=>{
+        res.json(data);
+    })
 })
 
 // Listening for a post request
@@ -52,6 +85,14 @@ app.post('/api/movies', (req, res)=>{
     console.log(req.body.title);
     console.log(req.body.year);
     console.log(req.body.poster);
+
+    MovieModel.create({
+        title:req.body.title,
+        year:req.body.year,
+        poster:req.body.poster
+    })
+
+    res.send('Item added')
 })
 
 app.listen(port, () => {
--- src/components/read.js
+++ src/components/read.js
@@ -13,7 +13,7 @@ export class Read extends React.Component {
     componentDidMount() {
         axios.get('http://localhost:4000/api/movies')
             .then((response) => {
-                this.setState({ movies: response.data.movies })
+                this.setState({ movies: response.data })
             })
             .catch((error) => {
                 console.log(error)
