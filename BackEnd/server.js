952b67adab81a05f5af82d9fb9766db696dbe62c
johngroves1 <G00367771@gmit.ie>
2020-11-29 19:47:13 +0000

    lab upload

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -79,6 +79,14 @@ app.get('/api/movies/:id', (req, res)=>{
     })
 })
 
+app.delete('/api/movies/:id',(req, res)=>{
+    console.log("Delete Movie: "+ req.params.id);
+
+    MovieModel.findByIdAndDelete(req.params.id,(err, data)=>{
+        res.send(data);
+    })
+})
+
 // Listening for a post request
 app.post('/api/movies', (req, res)=>{
     console.log('Movie received!');
--- src/components/movieItem.js
+++ src/components/movieItem.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 export class MovieItem extends React.Component {
+
+    constructor(){
+        super();
+
+        this.DeleteMovie = this.DeleteMovie.bind(this)
+    }
+
+    DeleteMovie(e){
+        e.preventDefault();
+        console.log("Delete: "+this.props.movie._id);
+
+        axios.delete("http://localhost:4000/api/movies/"+this.props.movie._id)
+        .then(()=>{
+            this.props.ReloadData();
+        })
+        .catch();
+    }
 
     render() {
         return (
@@ -18,6 +37,7 @@ export class MovieItem extends React.Component {
                             </footer>
                         </blockquote>
                     </Card.Body>
+                    <Button variant="danger" onClick={this.DeleteMovie}>Delete</Button>
                 </Card>
             </div>
         );
